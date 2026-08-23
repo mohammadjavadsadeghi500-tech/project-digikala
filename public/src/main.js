dcb356487c62eae81f523c6e8550dcf7fc14222f
Mohammad <mohammadjavadsadeghi500@gmail.com>
2026-08-24 01:14:38 +0330

    slider-2 almost done

--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,8 @@
 // first slider start
-
 const progressCircle = document.querySelector(".autoplay-progress svg");
 const progressContent = document.querySelector(".autoplay-progress span");
-var swiper = new Swiper(".mySwiper", {
+
+var swiper1 = new Swiper(".mySwiper-1", {
   spaceBetween: 30,
   centeredSlides: true,
   autoplay: {
@@ -24,5 +24,38 @@ var swiper = new Swiper(".mySwiper", {
     },
   },
 });
-
 // first slider end
+
+// slider 2 start
+function toggleSlider2PrevButton(swiper) {
+  const prevBtn = swiper.el.querySelector(".swiper-button-prev");
+  if (!prevBtn) return;
+  if (swiper.isBeginning) {
+    prevBtn.classList.remove("is-visible");
+  } else {
+    prevBtn.classList.add("is-visible");
+  }
+}
+
+var swiper2 = new Swiper(".mySwiper-2", {
+  slidesPerView: "auto",
+  centeredSlides: false,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  on: {
+    init: function () {
+      toggleSlider2PrevButton(this);
+    },
+    slideChange: function () {
+      toggleSlider2PrevButton(this);
+    },
+  },
+});
+// slider 2 end
