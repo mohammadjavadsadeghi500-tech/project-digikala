b4a435822fd15ce2d2be0d6df951336b301cba61
Mohammad <mohammadjavadsadeghi500@gmail.com>
2026-09-18 20:06:14 +0330

    page 2 start

---
+++ public/src/main2.js
@@ -0,0 +1,19 @@
+// products slider  s2
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 12,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+// slider amazing end 
+var swiper = new Swiper(".mySwiper-amazing-end", {
+  slidesPerView: 8,
+  spaceBetween: 0,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
